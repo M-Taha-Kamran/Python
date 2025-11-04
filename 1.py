626e10e0b396e01f12ed1be73a9ba8c68c2d1878
M-Taha-Kamran <muhammadtaha.kamran1343@gmail.com>
2025-11-04 23:25:32 +0500

    practise day 1

--- 1.py
+++ 1.py
@@ -1,3 +1,69 @@
-print("Hello World!")
+# print("Hello World!")
 
-print("M.Taha Kamran")
+
+
+
+
+# # Float
+# a = 10.122356676889789345245
+# b = -10.122356676889789345245
+
+
+# # Integer
+# c = 12323
+# d = -232
+
+# # String
+# name = "Name12346548987"
+# taha_name = 'taha kamran'
+
+# # Boolean 0 or 1
+# e = True
+# f = False
+
+# # List
+# g = [123, True, c, taha_name]
+# h = [33333,22222,44444,55555,66666]
+
+# # Dictionary
+# i = {"key": 123, 
+#      "key2": 123.123, 
+#      "key3": True}
+
+#######################################################
+
+
+# # 1. Define a list of Booleans
+# a = [True,False,True,False]
+# print(a)
+
+# # 2. Define two variables of your name and your father name
+# name = "M.Taha Kamran"
+# fathers_name = "kamran Umar"
+
+# print(name)
+# print(fathers_name)
+
+# # 3. Add two numbers a and b and print their result
+# b = 44
+# c = 55
+
+# d = b+c
+
+# print(d)
+
+
+# # 4. Create two strings "your_name" and "father_name" and add them
+
+# your_name = "M.Taha kamran" 
+# father_name = " Kamran Umar "
+# age = "15"
+# full_name = your_name + father_name + age
+
+# print(full_name)
+
+# 5. Create two float variables and multiple them, then print their result
+e = 5.6
+f = 5.55 
+g = e*f 
+print("Result: ", g)
